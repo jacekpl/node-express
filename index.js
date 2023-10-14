7c040cf9dfdb7d41fe25dce5a2ac2f71196b6a1c
Jacek Różański <jacek@opcode.pl>
2023-10-14 20:57:57 +0200

    Exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+morgan.token('body', function (req, res) {
+    if(req.method !== 'POST') {
+        return '';
+    }
+
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
