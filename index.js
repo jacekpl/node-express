5e05aca604e7bc13625f11c89bc602dbd11657b1
Jacek Różański <jacek@opcode.pl>
2023-10-15 16:41:48 +0200

    Exercise 3.22

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
-morgan.token('body', function (req, res) {
+morgan.token('body', function (req) {
     if (req.method !== 'POST') {
-        return '';
+        return ''
     }
 
     return JSON.stringify(req.body)
@@ -21,13 +21,16 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 const Person = require('./models/person')
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
+    Person.find({ }).then(persons => {
         response.json(persons)
     })
 })
 
-app.get('/info', (request, response) => {
-    response.send('<p>phonebook has info for ' + persons.length + ' people</p><p>' + new Date() + '</p>')
+app.get('/info', (request, response, next) => {
+    Person.count().then(count => {
+        response.send('<p>phonebook has info for ' + count + ' people</p><p>' + new Date() + '</p>')
+            .catch(error => next(error))
+    })
 })
 
 
@@ -43,7 +46,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
-        .then(result => {
+        .then(() => {
             response.status(204).end()
         })
         .catch(error => next(error))
@@ -51,16 +54,8 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    // const exists = persons.find(person => person.name === body.name)
-    //
-    // if (exists) {
-    //     return response.status(400).json({error: 'name must be unique'})
-    // }
-
     const person = new Person({
-        name: body.name,
-        number: body.number,
+        name: body.name, number: body.number,
     })
 
     person.save().then(savedPerson => {
@@ -70,9 +65,9 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
+    const { name, number } = request.body
 
-    Person.findByIdAndUpdate(request.params.id, {name, number}, {new: true, runValidators: true, context: 'query'})
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -84,9 +79,9 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
+        return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -4,7 +4,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
+    .then(() => {
         console.log('connected to MongoDB')
     })
     .catch((error) => {
@@ -33,8 +33,8 @@ const personSchema = new mongoose.Schema({
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString(),
-            delete returnedObject._id,
-            delete returnedObject.__v
+        delete returnedObject._id,
+        delete returnedObject.__v
     }
 })
 
