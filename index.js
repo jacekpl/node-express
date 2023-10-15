97c0a403d0fddb57f523da140ee9e643cd57406e
Jacek Różański <jacek@opcode.pl>
2023-10-15 10:24:47 +0200

    Exercise 3.13 + 3.14

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
+const cors = require('cors')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', function (req, res) {
-    if(req.method !== 'POST') {
+    if (req.method !== 'POST') {
         return '';
     }
 
     return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+const Person = require('./models/person')
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -53,17 +32,9 @@ app.get('/info', (request, response) => {
 
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const person = persons.find(person => {
-        return person.id === id
+    Person.findById(request.params.id).then(result => {
+        response.json(result)
     })
-
-    if (person) {
-        response.json(person)
-    }
-
-    response.status(404).end()
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -79,28 +50,26 @@ app.post('/api/persons', (request, response) => {
     const body = request.body
 
     if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number is missing'
-        })
+        return response.status(400).json({error: 'name or number is missing'})
     }
 
-    const exists = persons.find(person => person.name === body.name)
+    // const exists = persons.find(person => person.name === body.name)
+    //
+    // if (exists) {
+    //     return response.status(400).json({error: 'name must be unique'})
+    // }
 
-    if (exists) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        id: getRandomIntInclusive(1000,9999),
+    const person = new Person({
         name: body.name,
         number: body.number,
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
